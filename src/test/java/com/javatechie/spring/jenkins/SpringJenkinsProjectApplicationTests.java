2a9e2e716a28f09bd0ebf10b81d1b1e0ed89af5e
chandu <chandubanoth980@gmail.com>
2021-10-25 22:03:20 +0530

    first commit

---
+++ src/test/java/com/javatechie/spring/jenkins/SpringJenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
